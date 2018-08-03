f9ae349dbd01dd4a5c8953ac1be31f9a67c5a412
StrangeAlmond <echurch05@yahoo.com>
2018-08-02 22:36:49 -0700

    small change to kiss command

--- commands/kiss.js
+++ commands/kiss.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
-    if(!args[0]) return message.channel.send("Please specify someone to hug!");
+    if(!args[0]) return message.channel.send("Please specify someone to kiss!");
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!kUser) return message.channel.send("Can't find user!");
     if(kUser.id === message.author.id) return message.channel.send(`<@${message.author.id}> kissed him self!`);
