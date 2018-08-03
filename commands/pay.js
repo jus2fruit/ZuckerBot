e6f74ae2f9c38ce0686d63a8440aa83d9d2a8cc9
StrangeAlmond <echurch05@yahoo.com>
2018-08-02 22:31:58 -0700

    Small changes to pay command

--- commands/pay.js
+++ commands/pay.js
@@ -7,9 +7,10 @@ module.exports.run = async (bot,message,args) => {
 if(!coins[message.author.id]){
     return message.reply("You don't have any coins!")
 }
-
+if(!args[0]) return message.channel.send("Please specify a user to pay!");
 let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-
+if(!pUser) return message.channel.send("I couldn't find that user!")
+if(!args[1]) return message.channel.send("Please specify how much you want to pay!");
 if(!coins[pUser.id]){
     coins[pUser.id] = {
 coins: 0
